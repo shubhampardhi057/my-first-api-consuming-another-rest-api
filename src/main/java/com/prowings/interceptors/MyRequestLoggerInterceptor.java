821fd783a5a0be994c80eb65c085169463a7dc34
shubhampardhi057 <shubhampardhi057@gmail.com>
2023-09-02 16:46:30 +0530

    commit - 02/09/2023

--- src/main/java/com/prowings/interceptors/MyRequestLoggerInterceptor.java
+++ src/main/java/com/prowings/interceptors/MyRequestLoggerInterceptor.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpRequest;
@@ -18,6 +19,13 @@ public class MyRequestLoggerInterceptor implements ClientHttpRequestInterceptor 
 			throws IOException {
 		
 		System.out.println("Http Method and URL :"+ request.getMethod()+" "+request.getURI());
+		
+		UUID txnId = UUID.randomUUID();
+		
+		String transationId = txnId.toString();
+		
+		request.getHeaders().add("transactionId",transationId);
+		
 		System.out.println("Headers :"+ request.getHeaders());
 		System.out.println(">>> Request Body :"+ new String(body,StandardCharsets.UTF_8));
 		
